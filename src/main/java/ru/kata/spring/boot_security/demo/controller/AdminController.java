d64eb6d6f4dd1e9c8f799036c77ffc72820b3352
Ilya Krasilov <ilyakrasilov10@gmail.com>
2022-05-09 17:02:19 +0300

    v.1.0_PP_3_1_3_worked

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -34,20 +34,23 @@ public class AdminController {
     //Страница с ролью доступа ROLE_ADMIN:
     @GetMapping()
     public String showAllUsers(Model model, Principal principal) {
-        User user = userService.findByEmail(principal.getName());
+        User admin = userService.findByEmail(principal.getName());
         model.addAttribute("users", userService.getAllUsers());
-        model.addAttribute("user", user);
+        model.addAttribute("admin", admin);
+        model.addAttribute("roles", roleService.getAllRoles());
         return "admin";
     }
 
     //Ссылка на форму для создания User с ролью доступа ROLE_ADMIN
-    @GetMapping("/new")
+    /*@GetMapping("/new")
     public String createUserForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
         model.addAttribute("roles", roleService.getAllRoles());
         return "new";
     }
+
+     */
 
     //Сервис для создания User
     @PostMapping()
@@ -59,22 +62,14 @@ public class AdminController {
     }
 
     //Удаление пользователя из БД
-    @GetMapping("/{id}")
+    @PostMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteById(id);
         return "redirect:/admin";
     }
 
 
-    @GetMapping("/edit/{id}")
-    public String updateUserForm(@PathVariable Long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getAllRoles());
-        return "edit";
-    }
-
-    @PostMapping("/{id}")
+    @PostMapping("/edit/{id}")
     public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id,
                              @RequestParam(value = "rolesList", required = false) String roles) {
         user.setRoles(roleService.getRole(roles));
